b8bfd58d77a1fab3da9216915a20d8bc5ba915c3
David <dbjarki@gmail.com>
2017-11-12 20:06:01 +0000

    Setti inn prototype heimasíðu

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from bottle import *
+from pymysql import *
 
 @route("/")
 def home():
-  return "Buen? ? og Dav Dav eru komnar á Heroku"
+  return template('Main.tpl')
 
 if os.environ.get("IS_HEROKU") is not None:
     run(host="0.0.0.0", port=os.environ.get("PORT"))
